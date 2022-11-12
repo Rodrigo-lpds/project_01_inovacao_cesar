2f7faa5edc539cecc1a7c2ceb4b2bcc2fd983c4b
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-12 16:04:01 -0300

    add shopping products

---
+++ shopping.rb
@@ -0,0 +1,9 @@
+def products
+	products = {
+		"1" => { "name" => "ProdutoA", "price" => 50.00 },
+		"2" => { "name" => "ProdutoB", "price" => 200.00 },
+		"3" => { "name" => "ProdutoC", "price" => 100.00 }
+	}
+	
+	products
+end
